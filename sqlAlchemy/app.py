9d0eb090f9aa394b3d27dc16ea90d0d1bf341b80
SyedKashif512 <kashif.shah@usa.edu.pk>
2024-09-18 21:48:22 +0500

    filters added where filter or and

--- sqlAlchemy/app.py
+++ sqlAlchemy/app.py
@@ -1,4 +1,5 @@
 import random
+from sqlalchemy import or_, and_
 from sqlalchemy.orm import sessionmaker
 from models import engine, User
 
@@ -9,9 +10,9 @@ session = Session()
 
 names = ['Ali', 'Hassan', 'Syed Kashif Naqvi', 'Sara', 'Fatima', 'Zain', 'Imran', 'Baneen', 'Hamza', 'Bilal', 'Mariam', 'Kashif', 'Yasmin', 'Ibrahim']
 ages = [12, 29, 10, 54, 78, 12, 21, 45, 34, 18, 60, 32, 25, 41, 23, 30, 27]
-
+address = ['Lahore, Pakistan', 'Karachi']
 for item in range(20):
-    user = User(name=random.choice(names), address=random.choice(ages))
+    user = User(name=random.choice(names), address=random.choice(address), age=random.choice(ages))
     # session.add(user)
     # session.commit()
 
@@ -20,5 +21,28 @@ users = session.query(User).order_by(User.name, User.address)
 for user in users:
     user_information = f"""
     Hey my name is {user.name}. I live in {user.address}.
+    """
+    # print(user_information)
+
+
+
+# Data filters or and where
+users = session.query(User).where(or_(User.address == 'Karachi', User.age >= 30)).all()
+for user in users:
+    user_information = f"""
+    Hey my name is {user.name}. I live in {user.address} My age is {user.age}.
+    """
+    # print(user_information)
+
+# Data filters and ,or, and, filter
+users = session.query(User).filter(
+    or_(
+        and_(User.age > 20),
+        or_(User.address == 'Karachi')
+    )
+).all()
+for user in users:
+    user_information = f"""
+    Hey my name is {user.name}. I live in {user.address} My age is {user.age}.
     """
     print(user_information)
