c17940e95371cb1b33fa87a4c2589c7416884ab7
SyedKashif512 <kashif.shah@usa.edu.pk>
2024-09-23 23:36:25 +0500

    one to many field added with backpopulate

--- sqlAlchemy/models.py
+++ sqlAlchemy/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 db_url = "sqlite:///database.db"
 
@@ -7,12 +7,30 @@ engine = create_engine(db_url)
 
 Base = declarative_base()
 
-class User(Base):
+class BaseModel(Base):
+    __abstract__= True
+    __allow_unmapped__ = True
+    id = Column(Integer, primary_key=True)
+
+class Address(BaseModel):
+    __tablename__ = "addresses"
+    city = Column(String) 
+    state = Column(String)
+    zip_code = Column(Integer)
+    user_id = Column(ForeignKey('users.id'))
+    user = relationship('User', back_populates='addresses')
+    def __repr__(self):
+        return f"<Address(id={self.id}, city={self.city})>"
+
+class User(BaseModel):
     __tablename__ = "users"
     id = Column(Integer ,primary_key=True)
     name = Column(String, nullable=True)
     address = Column(String, nullable=True)
     age = Column(String, nullable=True)
     is_deleted = Column(Boolean, default=False)
-
+    addresses = relationship(Address, back_populates='user')
+    def __repr__(self):
+        return f"<User id={self.id}, name={self.name}>"
+    
 Base.metadata.create_all(engine)
